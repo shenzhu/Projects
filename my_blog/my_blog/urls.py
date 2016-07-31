b023f3cd8ca5554c97ff7ea92c914d384c7466e8
Shen Zhu <shenzhu0127@gmail.com>
2016-07-31 14:08:24 +0800

    dynamic url, archives, search, pagination

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -8,5 +8,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'article.views.home'),
-    url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^archives/$', 'article.views.archives', name='archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+    url(r'^search/$', 'article.views.blog_search', name = 'search'),
 )
